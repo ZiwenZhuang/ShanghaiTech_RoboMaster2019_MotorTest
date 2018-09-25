f96c412cfabf1e7f212fb5e4b11c04249c524446
ZiwenZhuang <leo416850130@outlook.com>
2018-09-25 16:16:31 +0800

    Prepare to make this project Arduino like
    This is aimed for easy testing without any code regulations

--- Src/main.c
+++ Src/main.c
@@ -51,69 +51,23 @@
 #include "Remote_Control.h"
 /* USER CODE END Includes */
 
-/* Private variables ---------------------------------------------------------*/
+/* Private variables */
 
 /* USER CODE BEGIN PV */
-/* Private variables ---------------------------------------------------------*/
-PID_TypeDef motor_pid[4];
-PID_TypeDef motor_position_pid[4]; // M2006驱动 '4' is only for the alignment
+/* Private variables */
 int32_t set_spd = 0;
-static int key_sta = 0;
-int speed_step_sign = +1;
 
 uint16_t TIM_COUNT[2];
 #define SpeedStep 500
 
 /* USER CODE END PV */
 
-/* Private function prototypes -----------------------------------------------*/
+/* Private function prototypes */
 void SystemClock_Config(void);
 
 /* USER CODE BEGIN PFP */
-/* Private function prototypes -----------------------------------------------*/
+/* Private function prototypes */
 
-/* USER CODE END PFP */
-
-/* USER CODE BEGIN 0 */
-void Key_Scan(){
-		
-		if(HAL_GPIO_ReadPin(KEY_GPIO_Port,KEY_Pin) == GPIO_PIN_RESET){
-						
-			if(key_sta == 0){
-					
-				key_sta = 1;
-				
-				set_spd += SpeedStep*speed_step_sign;
-				
-				if(set_spd>8000)
-				{
-					speed_step_sign = -1;
-				}
-				
-				if(set_spd<=0){
-						
-					set_spd = 0;
-					speed_step_sign = 1;
-					
-				}
-					
-			}
-			
-		}else{
-			
-			key_sta = 0;
-		
-		}
-	
-}
-
-
-
-
-
-/* USER CODE END 0 */
-
-/* USER CODE BEGIN 1 */
 /** @brief initialize a series of TIM ports and start the given channel
 **/
 void TIMs_init() {
@@ -124,6 +78,12 @@ void TIMs_init() {
   MX_TIM5_Init();
   MX_TIM8_Init();
   MX_TIM12_Init();
+}
+
+/* USER FUNCTION BEGIN vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv*/
+/** @brief start given PWM ports
+**/
+void TIMs_start() {
 	/* start specific pin port onboard */
 	HAL_TIM_PWM_Start(&htim5,TIM_CHANNEL_3);
 	HAL_TIM_PWM_Start(&htim5,TIM_CHANNEL_2);
@@ -139,7 +99,7 @@ void PWM_SetDuty(TIM_HandleTypeDef *tim,uint32_t tim_channel,float duty){
 		case TIM_CHANNEL_4: tim->Instance->CCR4 = value;break;
 	}
 }
-/* USER CODE END 1 */
+/* USER FUNCTION ENDs ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
 int main(void)
 {
 
@@ -147,21 +107,14 @@ int main(void)
 
   /* USER CODE END 1 */
 
-  /* MCU Configuration----------------------------------------------------------*/
+  /* MCU Configuration */
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  /* Reset of all peripherals, 
+	Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
 
   /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
@@ -169,48 +122,50 @@ int main(void)
   MX_CAN1_Init();
   MX_USART1_UART_Init();
 	TIMs_init();
-	// Now chaning the value in TIMx->CCRx (x refers to a number) should change the output of PWM port
+	// Now chaning the value in TIMx->CCRx 
+	// (x refers to a number) should change the output of PWM port
 
-  /* USER CODE BEGIN 2 */
   my_can_filter_init_recv_all(&hcan1);     //配置CAN过滤器
   HAL_CAN_Receive_IT(&hcan1, CAN_FIFO0);   //启动CAN接收中断
   HAL_UART_Receive_IT_IDLE(&huart1,UART_Buffer,100);   //启动串口接收
-
   HAL_TIM_IC_Start_DMA(&htim1,TIM_CHANNEL_2,(uint32_t *)TIM_COUNT,2);
 	
+	/* User initialization BEGIN vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv*/
+	TIMs_start();
 	/*< 初始化PID参数 >*/
+	PID_TypeDef motor_pid[4];
+	PID_TypeDef motor_position_pid[4]; // M2006驱动 '4' is only for the alignment
   for(int i=0; i<4; i++)
-  {	
-
+  {
     pid_init(&motor_pid[i]);
     motor_pid[i].f_param_init(&motor_pid[i],PID_Speed,16384,5000,10,0,8000,0, \
 															4,0.05,0);
     pid_init(&motor_position_pid[i]); // M2006
 		motor_position_pid[i].f_param_init(&motor_position_pid[i], PID_Position, 65535, 65535, 0.5,10,5,0, \
 															0.5,0.2,0); // M2006
-
   }
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
 	float spd_target [4]; // 记录目标角度(轴)
 	float last_spd_target = 0;
 	float pwm_duty = 0.1f; // This is defined bwtween 0 ~ 1
 	PWM_SetDuty(&htim5, TIM_CHANNEL_3, pwm_duty);
 	PWM_SetDuty(&htim5, TIM_CHANNEL_2, pwm_duty);
+
+  /* User initialization ENDs ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv*/
   while (1)
   {	
-    if(HAL_GetTick() - Latest_Remote_Control_Pack_Time >500){   //如果500ms都没有收到遥控器数据，证明遥控器可能已经离线
+    if(HAL_GetTick() - Latest_Remote_Control_Pack_Time >500){
+			//如果500ms都没有收到遥控器数据，证明遥控器可能已经离线
 			last_spd_target = spd_target[0];
 			set_moto_current(&hcan1, 0, 0, 0, 0);   // loose the motor
 			PWM_SetDuty(&htim5, TIM_CHANNEL_3, 0.1f); // stop the fraction wheel
 			PWM_SetDuty(&htim5, TIM_CHANNEL_2, 0.1f); // stop the fraction wheel
 			HAL_Delay(10);
 			continue;
-    }else{ // 设定个部位的目标值
-      // set_spd = remote_control.ch4*8000; // 摇杆度数
+    }else{ // 设定各部位的目标值
+      // remote_control.ch4*8000; // 摇杆度数
 			float angle_ratio = 815; // Ratio between required angle and the actual output
 			if (remote_control.switch_left == 1) { // left up
 				pwm_duty = 0.13;
@@ -235,23 +190,20 @@ int main(void)
 		PWM_SetDuty(&htim5, TIM_CHANNEL_2, pwm_duty);
 		for(int i=0; i<4; i++){	
 			motor_position_pid[i].target = spd_target[i]; // M2006
-			motor_position_pid[i].f_cal_pid(&motor_position_pid[i], moto_chassis[i].total_angle); // M2006
+			motor_position_pid[i].f_cal_pid(&motor_position_pid[i], moto_chassis[i].total_angle);
+			
 			motor_pid[i].target = motor_position_pid[i].output; // M2006																							
-			motor_pid[i].f_cal_pid(&motor_pid[i], moto_chassis[i].speed_rpm);    //根据设定值进行PID计算。
+			motor_pid[i].f_cal_pid(&motor_pid[i], moto_chassis[i].speed_rpm);
+			//根据设定值进行PID计算。
 		}
-		set_moto_current(&hcan1, motor_pid[0].output,   //将PID的计算结果通过CAN发送到电机
-													motor_pid[1].output,
-													motor_pid[2].output,
-													motor_pid[3].output);
+		//将PID的计算结果通过CAN发送到电机
+		set_moto_current(&hcan1, motor_pid[0].output,
+														motor_pid[1].output,
+														motor_pid[2].output,
+														motor_pid[3].output);
 		HAL_Delay(10);      	//PID控制频率100HZ
-		
-  /* USER CODE END WHILE */
-
-  /* USER CODE BEGIN 3 */
-
   }
-  /* USER CODE END 3 */
-
+	/* USER CODE BEGIN WHILE ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
 }
 
 /** System Clock Configuration
