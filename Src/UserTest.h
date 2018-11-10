9b88811591d951702cdedef3b54082416e32de7d
ZiwenZhuang <zhuangzw@shanghaitech.edu.cn>
2018-11-10 13:59:30 +0800

    try to make UART communication test

--- Src/UserTest.h
+++ Src/UserTest.h
@@ -13,35 +13,13 @@ Parameters changing paths:
 		main.h: #define PWM_FREQUENCE xxxxxxxx
 ****************************************************************************/
 
+/* 4 Motor control parameters. */
+PID_TypeDef motor_position_pid[4];
+PID_TypeDef motor_speed_pid[4];
 
+float pwm_duty[4];
 /** @brief start given PWM ports
 **/
-
-static float angle_ratio = 815; // Ratio between required angle and the actual output
-PID_TypeDef motor_pid[4];
-PID_TypeDef motor_position_pid[4];
-float motor_speed_target [4]; // set for control motors speed directly.
-float motor_target [4]; // 记录目标角度(轴)
-float last_motor_target [4]; // start at 0
-/*	0: loader motor (can id: 1)
-		1: yaw motor (can id: 2)
-*/
-float yaw_pos(int16_t rc_reading) {
-	return rc_reading * 1500;
-}
-
-// This is defined bwtween 0 ~ 1; defined for fraction wheel
-float pwm_duty0 = 0.05f;
-float pwm_duty1 = 0.05f;
-
-float pitch_reading_ratio = 8000;
-float rc2pitchPWM(int16_t rc_reading) {
-	float output = 0.075f + (float)rc_reading / 36500 * 5;
-	if (output > 0.1) output = 0.1;
-	if (output < 0.05) output = 0.05;
-	return output;
-}
-
 void TIMs_start() {
 	/* start specific pin port onboard */
 	HAL_TIM_PWM_Start(&htim5,TIM_CHANNEL_3); // servo
@@ -66,60 +44,38 @@ void PWM_SetDuty(TIM_HandleTypeDef *tim,uint32_t tim_channel,float duty){
 **/
 void _init_() {
 	TIMs_start();
-	/*< 初始化PID参数 >*/
-  for(int i=0; i<4; i++)
-  {
-    pid_init(&motor_pid[i]);
-    motor_pid[i].f_param_init(&motor_pid[i],PID_Speed,16384,5000,10,0,8000,0, \
+	// Initialize modor pids
+	for (int i = 0; i < 4; i++) {
+		pid_init(&motor_speed_pid[i]);
+    motor_speed_pid[i].f_param_init(&motor_speed_pid[i],PID_Speed,16384,5000,10,0,8000,0, \
 															4,0.05,0);
-    pid_init(&motor_position_pid[i]); // M2006
-		motor_position_pid[i].f_param_init(&motor_position_pid[i], PID_Position, 65535, 65535, 0.5,10,5,0, \
-															0.5,0.2,0); // M2006
-  }
+	}
 	HAL_Delay(20); // wait for 20ms
 }
 /**	@brief Arduino like process of keep looping and you can perform testing.
 **/
 void _loop_() {
+		uint8_t send_buffer[2048];
 		if(HAL_GetTick() - Latest_Remote_Control_Pack_Time >500){
 			//如果500ms都没有收到遥控器数据，证明遥控器可能已经离线
-			for (int i=0; i<4; i++) last_motor_target[i] = motor_target[i];
 			set_moto_current(&hcan1, 0, 0, 0, 0);   // loose the motor
 			HAL_Delay(10);
 			return;
-    }else{ // 设定各部位的目标值
-      // remote_control.ch4*8000; // 摇杆度数
-			
-			// set chassis movement
-			mecanum_calc(remote_control.ch1 * -3,
-									remote_control.ch2 * 3,
-									remote_control.ch3 / 10,
-									motor_speed_target);
-			
-			// servo poss
-			if (remote_control.switch_left == 1) { // left up
-				pwm_duty0 = 0.05f;
-			} else if (remote_control.switch_left == 2) { // left down
-				pwm_duty0 = 0.1f;
-			}
-			if (remote_control.switch_right == 1) { // right up
-				pwm_duty1 = 0.1f;
-			} else if (remote_control.switch_right == 2) { // right down
-				pwm_duty1 = 0.05f;
-			}
+    }else{ 
+			memcpy(send_buffer, UART6_RxBuffer, 2048);
+			HAL_UART_Transmit(&huart6, send_buffer, 2048, 100);
     }
 		
 		// Proceed the operations
-		PWM_SetDuty(&htim5, TIM_CHANNEL_3, pwm_duty0);
-		PWM_SetDuty(&htim5, TIM_CHANNEL_1, pwm_duty1);
+		// PWM_SetDuty(&htim5, TIM_CHANNEL_3, pwm_duty[0]);
 		for(int i=0; i<4; i++){	
-			motor_pid[i].target = motor_speed_target[i]; // M2006																							
-			motor_pid[i].f_cal_pid(&motor_pid[i], moto_chassis[i].speed_rpm);
+			motor_speed_pid[i].target = 0;																						
+			motor_speed_pid[i].f_cal_pid(&motor_speed_pid[i], moto_chassis[i].speed_rpm);
 		}
 		//将PID的计算结果通过CAN发送到电机
-		set_moto_current(&hcan1, motor_pid[0].output,
-														motor_pid[1].output,
-														motor_pid[2].output,
-														motor_pid[3].output);
+		set_moto_current(&hcan1, motor_speed_pid[0].output,
+														motor_speed_pid[1].output,
+														motor_speed_pid[2].output,
+														motor_speed_pid[3].output);
 		HAL_Delay(10);      	//PID控制频率100HZ
 }
